445abc38a58c17b58102dd79433ae4cd65177693
tyt9 <tythy9@gmail.com>
2023-11-14 12:13:45 +0900

    Initial commit

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+  methods: {
+    clearTodo(){
+      this.$emit('removeAll');
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+  </div>
+</template>
+
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
+</style>
